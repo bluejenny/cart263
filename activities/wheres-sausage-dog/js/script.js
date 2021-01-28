530e931bdc887a380a7fd4d37b63fbfbe03939e4
jen <jlpoohachoff@gmail.com>
2021-01-27 22:34:11 -0800

    created the array of dogs and preloaded/pushed images to the array

--- activities/wheres-sausage-dog/js/script.js
+++ activities/wheres-sausage-dog/js/script.js
@@ -1,32 +1,51 @@
 "use strict";
 
 /**
-Title of Project
-Author Name
+Find the Sausage Dog
+Activity with Pippin Bar
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+**my version**
 */
 
-/**
-Description of preload
-*/
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 100;
+
+let animalImages = [];
+let animals = [];
+
+
 function preload() {
-
+  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
+    let animalImage = loadImage(`assets/images/animal${i}.png`);
+    animalImages.push(animalImage);
+  }
 }
 
 
-/**
-Description of setup
-*/
+
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  //create the animals
+  for (let i = 0; i < NUM_ANIMALS; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let animalImage = random(animalImages);
+    let animal = new Animal(x, y, animalImage);
+    animals.push(animal);
+  }
 
 }
 
 
-/**
-Description of draw()
-*/
-function draw() {
 
+function draw() {
+  background(255, 255, 0);
+  for (let i = 0; i < animals.length; i++) {
+    animals[i].update();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
