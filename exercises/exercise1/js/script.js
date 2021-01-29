e5167bdda5618e8b542ac215192bcf66bc60cbe9
jen <jlpoohachoff@gmail.com>
2021-01-28 16:14:55 -0800

    added feature of selecting a random image

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -1,14 +1,26 @@
 "use strict";
 
 /**
-Find the Sausage Dog
-Activity with Pippin Bar
+Find the Sausage Dog Activity with Pippin Bar
 
 **my version**
+
+Find a randomly selected image from the top of a shuffled Array of *Animal Images*
+
+- added sound when user find *Sausage Dog* for emphasis
+- added an intro screen to show the selected image to find
+
 */
 
 const NUM_ANIMAL_IMAGES = 10;
 const NUM_ANIMALS = 100;
+
+//to create a random colored screen on each load
+let bg = {
+  r: 0,
+  g: 0,
+  b: 0
+}
 
 let animalImages = [];
 let animals = [];
@@ -23,7 +35,7 @@ function preload() {
     animalImages.push(animalImage);
   }
 
-  sausageDogImage = loadImage(`assets/images/sausage-dog.png`)
+  //  = loadImage(`assets/images/sausage-dog.png`)
 }
 
 
@@ -31,18 +43,31 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
+  bg.r = random(0, 255);
+  bg.g = random(0, 255);
+  bg.b = random(0, 255);
+
+
+  animalImages = shuffle(animalImages);
+  sausageDogImage = animalImages.pop();
+
   createAnimals();
   createSausageDog();
+
+
+
 }
 
 
 function draw() {
-  background(255, 255, 0);
+  background(bg.r, bg.g, bg.b);
   for (let i = 0; i < animals.length; i++) {
     animals[i].update();
   }
 
-  sausageDog.update();
+
+  // sausageDog.update();
+  drawSausageDog();
 }
 
 function createAnimals() {
@@ -59,6 +84,14 @@ function createSausageDog() {
   let x = random(0, width);
   let y = random(0, height);
   sausageDog = new SausageDog(x, y, sausageDogImage);
+}
+
+function drawSausageDog() {
+
+    sausageDog.update();
+
+if(floor(frameCount/30)%2==0)
+    image(sausageDogImage, 0,0);
 }
 
 function mousePressed() {
