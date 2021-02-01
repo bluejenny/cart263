d97c9bb03f00619d1278f0d8c6774c3d458515cd
jen <jlpoohachoff@gmail.com>
2021-02-01 18:49:09 -0500

    Started Responsive Voice Tutorial

---
+++ experiments/responsivevoice/js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+Responsive Voice Experiment
+*/
+
+
+function preload() {
+
+}
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+
+function draw() {
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
