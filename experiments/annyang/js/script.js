2387fef16eb5d3175a5e0b46a665244348f53e7d
jen <jlpoohachoff@gmail.com>
2021-02-02 15:30:07 -0500

    working code for Slamina exercise

--- experiments/annyang/js/script.js
+++ experiments/annyang/js/script.js
@@ -27,14 +27,6 @@ function setup() {
 function draw() {
   background(0, 0, 200);
 }
-//
-// function mousePressed() {
-//   responsiveVoice.speak("hello world", "UK English Female", {
-//     pitch: 0.5,
-//     rate: 0.5,
-//     volume: 1
-//   });
-// }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
