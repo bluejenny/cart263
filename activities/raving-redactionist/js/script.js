89d33301c8ef788e12066a908f1d4c30a2dacf6f
jen <jlpoohachoff@gmail.com>
2021-03-14 22:21:50 -0400

    finished top secret raving redactionist activity

--- activities/raving-redactionist/js/script.js
+++ activities/raving-redactionist/js/script.js
@@ -5,4 +5,22 @@ by Pippin Barr
 
 "use strict";
 
-// Code goes here
+$(`.top-secret`).on(`click`, redact);
+setInterval(hackIt, 500);
+
+function redact(event) {
+  $(this).removeClass(`reveal`);
+  $(this).addClass(`redacted`);
+}
+
+function hackIt() {
+  $(`.redacted`).each(attemptReveal);
+}
+
+function attemptReveal() {
+  let r = Math.random();
+  if (r < 0.1) {
+    $(this).removeClass(`redacted`);
+    $(this).addClass(`reveal`);
+  }
+}
