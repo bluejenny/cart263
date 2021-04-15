fd9fc41ea6b5ac546da8065d7da558cdaf6b9f6b
jen <jlpoohachoff@gmail.com>
2021-04-15 18:32:32 -0400

    first draft of datepicker and submit working
    - need to make submit work on form modular
    - add default value to top date with time
    - assign block color value

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -6,88 +6,131 @@ Jennifer Poohachoff
 
 "use strict";
 
+// form inputs
 let datePicker = document.getElementById(`date-picker`);
 let colorPicker = document.getElementById(`color-picker`);
 let moodPicker = document.getElementById(`moods`);
 let alignmentPicker = document.getElementById(`alignment-picker`);
 let widthPicker = document.getElementById(`width-picker`);
 
-// Alert the date chosen each time it's changed
-datePicker.addEventListener(`change`, function(event) {
-  let date = event.target.value;
-  console.log(date);
-  // alert(date);
+// calendar blocks
+let blocks = [];
+let label = 0;
+
+//moods
+
+let moodDropdown = $("#moods");
+
+
+// new date picker for experiment
+
+var $datepicker = $('#datepicker');
+$datepicker.datepicker();
+$datepicker.datepicker('setDate', new Date());
+
+// Add json file to <select> dropdown menu
+// https://www.codebyamir.com/blog/populate-a-select-dropdown-list-with-json
+moodDropdown.empty();
+moodDropdown.append('<option selected="true" disabled>I am feeling:');
+moodDropdown.prop("selectedIndex", 0);
+
+const url = "../assets/json/moods.json";
+
+$.getJSON(url, function (data) {
+  for (let i = 0; i < data.moods.length; i++) {
+    moodDropdown.append(
+      $("<option></option>").attr("value", data.moods[i]).text(data.moods[i])
+    );
+  }
 });
 
-// Set the background color of the document when the color
-// picker is used
-colorPicker.addEventListener(`input`, function(event) {
+
+// // listen for date input
+// datePicker.addEventListener(`change`, function (event) {
+//   let date = event.target.value;
+//   console.log(date);
+//   // alert(date);
+// });
+//
+// listen for colorpicker and change the background color when color is selected
+colorPicker.addEventListener(`input`, function (event) {
   let color = event.target.value;
   document.body.style[`background-color`] = color;
   console.log(color);
 });
-
-// Alert the date chosen each time it's changed
-moodPicker.addEventListener(`change`, function(event) {
-  let mood = event.target.value;
-  // alert(date);
-  console.log(mood);
-});
-
-// Alert the date chosen each time it's changed
-widthPicker.addEventListener(`change`, function(event) {
-  let colorWidth = event.target.value;
-  // alert(date);
-  console.log(colorWidth);
-});
-
-// Alert the date chosen each time it's changed
-alignmentPicker.addEventListener(`change`, function(event) {
-  let alignment = event.target.value;
-  // alert(date);
-  console.log(alignment);
-});
-
-
-
-
+//
+// // listen for mood picker and store word
+// moodPicker.addEventListener(`change`, function (event) {
+//   let mood = event.target.value;
+//   // alert(date);
+//   console.log(mood);
+// });
+//
+// // listen for color width and store width
+// widthPicker.addEventListener(`change`, function (event) {
+//   let colorWidth = event.target.value;
+//   // alert(date);
+//   console.log(colorWidth);
+// });
+//
+// // listen for alignment
+// alignmentPicker.addEventListener(`change`, function (event) {
+//   let alignment = event.target.value;
+//   // alert(date);
+//   console.log(alignment);
+// });
+//
 $(`#input-form`).dialog({
   resizable: false,
   modal: false,
   buttons: {
-    "Submit": function() {
+    Submit: function () {},
+  },
+});
 
+$(`#add`).on(`click`, addBlock);
+
+// Get timestamp
+function addBlock() {
+  let date = $(`#datepicker`).val();
+  let timestamp = Date.parse(date);
+  console.log(timestamp);
+  console.log(date);
+
+  // Create the block
+  let $block = $(`<div id=${timestamp}></div>`);
+  $block.css({
+    width: `${25 + Math.random() * 100}px`,
+    height: `100px`,
+    backgroundColor: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
+    float: `left`,
+  });
+  $block.text(label);
+  label++;
+
+  // Search for where to add the new block based on its timestamp
+  let index = blocks.length;
+  for (let i = 0; i < blocks.length; i++) {
+    if (timestamp < blocks[i]) {
+      index = i;
+      break;
     }
   }
-});
 
-$( ".prison" ).draggable();
+  // Add the block HTML element to the page in the right position
+ if ($(`#${blocks[index]}`).length === 0) {
+   $(`#calendar`).append($block)
+ }
+ else {
+   $(`#${blocks[index]}`).before($block);
+ }
+
+ // Adding the new timestamp to the array
+ blocks.splice(index,0,timestamp);
+}
+
+$(".prison").draggable();
 // $( "#prison" ).draggable({ containment: "parent" });
-
-
-
-
-/**
-Add json file to <select> dropdown menu
-https://www.codebyamir.com/blog/populate-a-select-dropdown-list-with-json
-
-*/
-
-let dropdown = $('#moods');
-
-dropdown.empty();
-
-dropdown.append('<option selected="true" disabled>I am feeling:');
-dropdown.prop('selectedIndex', 0);
-
-const url = '../assets/json/moods.json';
-
-$.getJSON(url, function (data) {
-  for (let i = 0; i < data.moods.length; i++) {
-     dropdown.append($('<option></option>').attr('value', data.moods[i]).text(data.moods[i]));
-  }
-});
-
 
 /**
 create form button to open and close
